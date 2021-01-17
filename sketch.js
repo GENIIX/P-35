6819273c71a6ccaf193f6310ec6cc0892b085167
GENIIX <75194523+GENIIX@users.noreply.github.com>
2021-01-17 14:09:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+//Create variables here
+ var dog, happyDog, database, foodS, foodStock
+var dogIMG,happyDogIMG;
+
+
+
+
+function preload()
+{
+  //load images here
+  dogIMG=loadImage("dogIMG.png");
+  happyDogIMG=loadImage("happyDog.png");
+}
+
+function setup() {
+  createCanvas(800, 700);
+  
+
+  dog=createSprite(400,350,50,50);
+  dog=addImage(dog.png)
+
+
+
+
+}
+
+
+function draw() {  
+background("crimson")
+  drawSprites();
+  //add styles here
+
+}
+
+
+
